cb5d61cedd510a5465d3722bc3a54e2a8aee9a45
Fatih Solhan <fatih.solhan@hotmail.com>
2022-04-30 01:12:27 +0300

    chore: added missing style.css file to demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -27,6 +27,7 @@
 <script lang="ts">
 import { ComponentPublicInstance, computed, defineComponent, onMounted, ref } from 'vue';
 import { useVOnboarding, VOnboardingWrapper } from 'v-onboarding';
+import 'v-onboarding/dist/style.css';
 import CatType from '../types/CatType';
 import AppButton from './components/AppButton.vue';
 import AppHeader from './components/AppHeader.vue';
