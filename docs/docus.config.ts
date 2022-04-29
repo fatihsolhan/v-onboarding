736b071b750a571ca219beefcb10cc320e2dc3f2
Fatih Solhan <fatih.solhan@hotmail.com>
2022-04-29 22:50:45 +0300

    created docs

---
+++ docs/docus.config.ts
@@ -0,0 +1,25 @@
+export default {
+  title: 'v-onboarding',
+  url: 'https://www.github.com/fatihsolhan/v-onboarding',
+  theme: {
+    colors: {
+      primary: '#8E75FF',
+      prism: {
+        background: '#F4F4F5 dark:#1F2937'
+      }
+    },
+    header: {
+      title: false,
+      logo: {
+        light: '/logo-light.png',
+        dark: '/logo-dark.png'
+      }
+    }
+  },
+  twitter: '@fatihsolhann',
+  github: {
+    repo: 'fatihsolhan/v-onboarding',
+    branch: 'main',
+    releases: false
+  }
+}
