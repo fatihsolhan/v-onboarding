3486ae65e23d50ecfbe93c546f906f6417ae3d1c
productdevbook <hi@productdevbook.com>
2022-11-05 08:03:35 +0300

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import VOnboardingStep from './components/VOnboardingStep.vue';
 import VOnboardingWrapper from './components/VOnboardingWrapper.vue';
 import useVOnboarding from './composables/useVOnboarding';
+export * from './types'
 import './css/v-onboarding.sass';
 export {
   VOnboardingWrapper,
---
+++ src/types/index.ts
@@ -0,0 +1,2 @@
+export type {AttachableElement,StepEntity} from './StepEntity'
+export type { VOnboardingWrapperOptions, SvgOverlayOptions } from './VOnboardingWrapper'
